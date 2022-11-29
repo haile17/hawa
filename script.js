235239a88379737b594061882492db1d3179b226
Haile Fikere <hailefikere17@gmail.com>
2022-11-29 16:20:04 +0300

    update

--- script.js
+++ script.js
@@ -31,9 +31,3 @@ navToggler.addEventListener('click', function () {
 AOS.init({
 	duration: 1000
 });
-
-// When the user clicks on div, open the popup
-function myFunction() {
-	var popup = document.getElementById("myPopup");
-	popup.classList.toggle("show");
-}
