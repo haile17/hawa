2b46b732f1a765736bbffdf9c98dce92354659fb
Haile Fikere <hailefikere17@gmail.com>
2022-12-06 14:53:07 +0300

    update

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 function myMap() {
     var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
+      center:new google.maps.LatLng(9.000525,38.748322),
+      zoom:9,
+      mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     }
